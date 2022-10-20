75544bc994e91096c697134bd11f9b49f4e01cf1
Brakinveste <brakinveste@gmail.com>
2022-10-19 22:01:16 -0300

    commit01

---
+++ main.js
@@ -0,0 +1,126 @@
+function fetchApiData() {
+    fetch("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            const list = document.querySelector('#prod_list');
+
+            data.map((item) => {
+                const li = document.createElement('li');
+
+                li.setAttribute('id', item.id);
+                li.innerHTML = item.name;
+                list.appendChild(li);
+            })
+
+        })
+         
+   
+}
+
+
+
+
+
+//    https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1
+
+
+//   let usuarios = JSON.parse(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function fazGet(url) {
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+
+}
+
+function criaLinha(usuario) {
+
+    console.log(usuario)
+    linha = document.createElement("tr");
+    tdId = document.createElement("td");
+    tdNome = document.createElement("td");
+    tdId.innerHtml = usuario.id
+    tdNome.innerHtml = usuario.nome
+
+    linha.appendChild(tdId);
+    linha.appendChild(tdNome);
+
+    return linha;
+
+}
+
+function main () {
+    
+    let data = fazGet("https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1")
+    let usuarios = JSON.parse(data);
+    let tabela = document.getElementById("tabela")
+   
+  
+    usuarios.forEach(element => {
+        let linha = criaLinha(element);
+        tabela.appendChild(linha)
+
+    });
+  
+    
+    console.log(usuarios)
+}
+
+main()
+
+*/
